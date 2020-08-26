2b2fb978c8f0d51e293619b9e30b899234947bff
Harry Tran <nhattq.coding@gmail.com>
2020-08-26 17:01:30 +0700

    161 feat(test): update method to testing vue app

--- components/okrs/okrs.constant.ts
+++ components/okrs/okrs.constant.ts
@@ -1,7 +1,10 @@
-export const customColors = (percentage: number): String => {
-  if (percentage < 30) {
+const lowPercent: number = 30;
+const mediumPercent: number = 70;
+
+export const customColors = (percentage: number): string => {
+  if (percentage < lowPercent) {
     return '#9f7aea'; // Purple-500
-  } else if (percentage < 70) {
+  } else if (percentage < mediumPercent) {
     return '#805ad5'; // Purple-600
   } else {
     return '#50248f'; // primary-purple-4
